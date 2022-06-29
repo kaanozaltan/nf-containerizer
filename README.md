600b4d9348c8f35ed18e7b8aba4b6bff2043d39c
kaanozaltan <ozaltankaan@gmail.com>
2022-06-29 14:53:43 +0300

    up tests

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Documentation
-playbook.yml: Builds and runs UDM as a container in the test-servers group hosts using the Dockerfile.
+## Containerization and Deployment
+This tool containerizes the following NFs with base image Ubuntu 16.04 using Docker and deploys them on the specified hosts using Ansible with the command `ansible-playbook -i test_hosts playbook.yml` where `test_hosts` is the inventory file with the group `test_hosts`:
+- NRF
+- UDR
+- UDM
+- AMF
+- SMF
+- AUSF
+- NSSF
+- UPF
+- NEF
+- PCF
+	- AMF
+	- NFRS
+	- PES
+	- OMS
+	- IWS
+	- SMS
+
+Diagram of the process:
+![Deployment diagram](diagram.png)
+
+When the playbook is run again, if the latest image is built for a CNF, it is assumed that a container of that image exists and the said container is started again if it is exited. If the images are removed, the build and run process is repeated.
+
+## Status and Port Testing
+After deployment, services of all NFs are checked for activity. Later, the specified ports inside `build-run.sh` are exposed in their respective containers and tested for connection through the host machine. The results are reported in a playbook task.
--- build-run.sh
+++ build-run.sh
@@ -1,19 +1,19 @@
 # ports
-cnrudr=(5400)
-cnrudm=(5000 5001 5002 5003 5004 5005)
-cnrnrf=(8001 8006 8007 8010)
-cnramf=(6210 6211 6212 6213 6286 6287)
-cnrsmf=(6110 6111 6122 6117 6112 6113 6121 6114 6115 6116 6118 6120 2152 6124 6123 6126 6127 6128 6129 6130 6990 6131 6133 6134 6135)
-cnrausf=(5500 5501 5502)
-cnrnssf=(8100 8101) # 6286
-cnrupf=(1800 1801 1802 1803 1804 8443)
-cnrpcfams=(7000)
-cnrpcfcs=(7050 7500)
-cnrpcfsms=(7002)
-cnrpcfnfrs=(7080)
-cnrpcfpes=(7008)
-cnrpcfoms=(7052)
-cnrpcfiws=(7020)
+cnrudr=()
+cnrudm=()
+cnrnrf=()
+cnramf=()
+cnrsmf=()
+cnrausf=()
+cnrnssf=()
+cnrupf=()
+cnrpcfams=()
+cnrpcfcs=()
+cnrpcfsms=()
+cnrpcfnfrs=()
+cnrpcfpes=()
+cnrpcfoms=()
+cnrpcfiws=()
 cnrnef=()
 
 res=$(docker image inspect $1:latest --format="exists")
@@ -38,16 +38,19 @@ if [ "$res" != "exists" ]; then
     done
 
     id=$(docker run -it -d --privileged=true $ports$1)
-    docker exec $id apt-get install -y $1
+    docker exec $id apt-get install -y $1 net-tools
     sleep 5
     ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id)
-    echo -e $"$1:" >> /home/report
+    docker exec $id systemctl restart $1
+    ctl=$(docker exec $id systemctl status cnr* | grep 'service -\|active')
+
+    echo -e $"$1:\n$ctl" >> /home/report
     for port in ${!var}; do
         nc -zv $ip $port >> /home/report 2>&1
     done
     echo -e $"" >> /home/report
 else
-    cid=$(docker ps --filter status=paused | grep $1 | awk '{ print $1 }')
+    cid=$(docker ps -f status=exited | grep $1 | awk '{ print $1 }')
     if [ "$cid" != "" ]; then
         docker start $cid
     fi
