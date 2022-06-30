f049dbc3b140f81c05798f2f7b069df66803c7f2
kaanozaltan <ozaltankaan@gmail.com>
2022-06-30 17:01:46 +0300

    settings

--- build-run.sh
+++ build-run.sh
@@ -16,6 +16,10 @@ cnrpcfoms=()
 cnrpcfiws=()
 cnrnef=()
 
+# nfs with settings.json
+has_settings(cnramf cnrausf cnrnrf cnrnssf cnrsmf cnrudm cnrudr cnrupf)
+declare -A translate=(["cnrnrf-vnf"]="nrf" ["cnramf"]="amf" ["cnrausf"]="ausf" ["cnrnssf"]="nssf" ["cnrsmf"]="smf" ["cnrudm"]="udm" ["cnrudr"]="udr" ["cnrupf"]="upf")
+
 res=$(docker image inspect $1:latest --format="exists")
 if [ "$res" != "exists" ]; then
     docker build -t $1 /home
@@ -40,11 +44,16 @@ if [ "$res" != "exists" ]; then
     id=$(docker run -it -d --privileged=true $ports$1)
     docker exec $id apt-get install -y $1 net-tools
     sleep 5
-    ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id)
-    docker exec $id systemctl restart $1
-    ctl=$(docker exec $id systemctl status cnr* | grep 'service -\|Active')
 
+    if [[ " ${has_settings[*]} " =~ " $1 " ]]; then
+        docker cp settings/${1}/settings.json /opt/cinar/${translate[$1]}
+    fi
+
+    # create report
+    docker exec $id systemctl restart $1
+    ctl=$(docker exec $id systemctl status cnr* | grep 'service -\|Active'
     echo -e $"$1:\n$ctl" >> /home/report
+    ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id)
     for port in ${!var}; do
         nc -zv $ip $port >> /home/report 2>&1
     done
